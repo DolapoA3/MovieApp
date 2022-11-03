293e2a22101f873ac0d4663b8d6474f6e5414630
LeoKing33 <lapo8387@gmail.com>
2022-11-02 20:12:51 -0500

    implementing search feature

--- src/App.js
+++ src/App.js
@@ -10,11 +10,26 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState('');
 
+  const getMovieRequest = async (searchValue) => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=efbaa381`;
+
+    const response = await fetch(url);
+    const responseJson = await response.json();
+
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
+  };
+
+  useEffect(() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
+
   return (
     <div className='container-fluid movie-app'>
-      <div className="row d-flex align-items-center mt-4 mb-4">
-        <MovieListHeading heading='Movies'/>
-        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue}/>
+      <div className='row d-flex align-items-center mt-4 mb-4'>
+        <MovieListHeading heading='Movies' />
+        <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
       <div className='row'>
         <MovieList movies={movies} />
